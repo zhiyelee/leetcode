a88b763b6d0f6a75bf18d04793e97cee5a5d73f6
zhiyelee <zhiyelee@gmail.com>
2015-01-10 21:57:23 +0800

    #7 ac

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,22 +1,24 @@
 class Solution:
     # @return an integer
     def reverse(self, x):
+        maxint = 2**31 - 1
         sign = 1
         if x < 0:
             sign = -1
-
         strx = str(abs(x))
         res = strx[::-1]
+        if (int(res)) > maxint:
+            return 0
         return sign*int(res)
 
 
 
 
-# # test
-# s = Solution()
-# x = 123
-# print s.reverse(0)
-# print s.reverse(100000)
-# print s.reverse(1212)
-# print s.reverse(-899)
-# print s.reverse(99989787387873**2)
+assert 1212 == 1212
+# test
+s = Solution()
+assert s.reverse(0) == 0
+assert s.reverse(100000) == 1
+assert s.reverse(1212) == 2121
+assert s.reverse(-899) == -998
+assert s.reverse(99989787387873**2) == 0
