cce176daaac38199a9394982bc7fdd141c6e3f99
zhiyelee <zhiyelee@gmail.com>
2015-01-12 15:39:49 +0800

    #2

---
+++ median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,39 @@
+class Solution:
+        # @return a float
+        def findMedianSortedArrays(self, A, B):
+            lenA = len(A)
+            lenB = len(B)
+            k = (lenA + lenB + 1)/2
+            if (lenA + lenB)%2 == 1:
+                return self.findK(A, B, k)
+            else:
+                return (self.findK(A, B, k) + self.findK(A, B, k + 1)) * 0.5
+
+            return 1
+        def findK(self, A, B, k):
+            lenA = len(A)
+            lenB = len(B)
+            # keep lenA <= lenB
+            if (lenA > lenB):
+                return self.findK(B, A, k)
+            if (lenA == 0):
+                return B[k-1]
+            if (k == 1):
+                return min(A[0], B[0])
+
+            midA = min(k/2, lenA)
+            midB = k - midA
+
+            if A [midA - 1] < B[midB - 1]:
+                return self.findK(A[midA:], B, midB)
+            else:
+                return self.findK(A, B[midB:], midA)
+
+
+s = Solution()
+assert s.findMedianSortedArrays([1,2,3,5], [2,5]) == 2.5
+assert s.findMedianSortedArrays([1,2,3,5], [2]) == 2
+assert s.findMedianSortedArrays([1,2,3,5], [8]) == 3
+assert s.findMedianSortedArrays([1,2], [1, 1]) == 1
+assert s.findMedianSortedArrays([2,3,4,5], [1]) == 3
+assert s.findMedianSortedArrays([2,3,4, 5, 6], [1]) == 3.5
