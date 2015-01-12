7ee5a86be4d22d56f3410096c23fcb15dce080d8
zhiyelee <zhiyelee@gmail.com>
2015-01-12 15:47:54 +0800

    #2 add comments

--- median_of_Two_Sorted_Arrays.py
+++ median_of_Two_Sorted_Arrays.py
@@ -3,16 +3,19 @@ class Solution:
         def findMedianSortedArrays(self, A, B):
             lenA = len(A)
             lenB = len(B)
+
+            # the mid for both even and odd
             k = (lenA + lenB + 1)/2
             if (lenA + lenB)%2 == 1:
                 return self.findK(A, B, k)
             else:
                 return (self.findK(A, B, k) + self.findK(A, B, k + 1)) * 0.5
 
-            return 1
+        # find the k-th smallest number
         def findK(self, A, B, k):
             lenA = len(A)
             lenB = len(B)
+
             # keep lenA <= lenB
             if (lenA > lenB):
                 return self.findK(B, A, k)
@@ -21,6 +24,8 @@ class Solution:
             if (k == 1):
                 return min(A[0], B[0])
 
+            # k/2
+            # get the k smallest numbers in A+B
             midA = min(k/2, lenA)
             midB = k - midA
 
@@ -29,7 +34,7 @@ class Solution:
             else:
                 return self.findK(A, B[midB:], midA)
 
-
+# tests
 s = Solution()
 assert s.findMedianSortedArrays([1,2,3,5], [2,5]) == 2.5
 assert s.findMedianSortedArrays([1,2,3,5], [2]) == 2
