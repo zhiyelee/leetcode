0a2fff34df0c6a4d774a3c542685fb6f8a97efa9
zhiyelee <zhiyelee@gmail.com>
2015-01-18 22:41:32 +0800

    #5 new version 141ms

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -1,29 +1,32 @@
 class Solution:
     # @return a string
     def longestPalindrome(self, s):
-        res = ''
-
-        for i in range(len(s)):
-            podd = self.getlongest(s, i, i)
-            if len(podd) > len(res):
-                res = podd
-
-            peven = self.getlongest(s, i, i + 1)
-            if len(peven) > len(res):
-                res = peven
-
-        return res
-    def getlongest(self, s, l, r):
         lens = len(s)
-        while l >= 0 and r < lens and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return s[l + 1 : r]
+        if (lens == 0):
+            return 0
+
+        maxLen = 1
+        sIdx = 0
+
+        for i in range(lens):
+            # when increase 1 element, the maxLen can add 1, 2, 0
+            # add 2, then compare s[i - maxLen -1] and i s[i - maxLen -1]
+            if i - maxLen >=1 and s[i-maxLen - 1: i+1] == s[i-maxLen - 1: i +1][::-1]:
+                sIdx = i - maxLen -1
+                maxLen += 2
+            # add 1, then compare s[i - maxLen] and i s[i - maxLen]
+            elif i - maxLen >= 0 and s[i - maxLen :i + 1] == s[i - maxLen :i + 1][::-1]:
+                sIdx = i - maxLen
+                maxLen += 1
+
+        return s[sIdx:sIdx + maxLen]
+
 
 
 # test
 
 s = Solution()
 
+print s.longestPalindrome('abcdedcba')
 assert s.longestPalindrome('abcdedcba') == 'abcdedcba'
 assert s.longestPalindrome('abcdedcbadddd') == 'abcdedcba'
