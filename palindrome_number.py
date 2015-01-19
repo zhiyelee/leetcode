4c7dd1898fa0959e407ee1e8d2229bf2f7a7bae0
zhiyelee <zhiyelee@gmail.com>
2015-01-19 23:57:30 +0800

    #9 updated version 429ms

--- palindrome_number.py
+++ palindrome_number.py
@@ -4,32 +4,26 @@ class Solution:
         if x < 0:
             return False
         if (x < 10):
-            return 1
+            return True
         prefix = x
         suffix = x % 10
-        lens = 1
+        p = 10
         # for 1213445443121
         # get x/10**n < 10**4 1213 and x%10**4 3121
         while suffix * suffix < x:
 
             prefix = x
-            while prefix >= self.power(lens):
+            while prefix >= p:
                 prefix = prefix / 10
 
-            l = suffix / (self.power(lens - 1))
+            l = suffix / (p/10)
             h = prefix % 10
             if (l != h):
                 return False
-            lens += 1
-            suffix = x % self.power(lens)
+            p = p * 10
+            suffix = x % p
 
         return True
-    def power(self, n):
-        res = 1
-        for i in range(n):
-            res *= 10
-
-        return res
 
 
 # test
@@ -38,9 +32,7 @@ s = Solution()
 
 assert s.isPalindrome(10) == False
 assert s.isPalindrome(1) == True
-assert s.isPalindrome(-1) == True
+assert s.isPalindrome(-1) == False
 assert s.isPalindrome(1213) == False
-assert s.isPalindrome(-2147447412) == False
 assert s.isPalindrome(12133121) == True
 assert s.isPalindrome(121313121) == True
-assert s.isPalindrome(-121313121) == True
