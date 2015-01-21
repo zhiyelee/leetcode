f32428d5fc6c94e2f75ee80a84e89c28b7e3720e
zhiyelee <zhiyelee@gmail.com>
2015-01-22 00:21:32 +0800

    wrong answer, found it is fibonacci

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -7,13 +7,18 @@ class Solution:
         count = 0
         for i in range(n + 1):
             m = n - i
-            if m % 2 == 0:
-                count += self.getAn_m(n, m/2)
+            if m == 0:
+                count += 1
+            elif m % 2 == 0:
+                count += self.getAn_m(i, m/2)
+                print i, m ,count
 
         return count
 
     # A_m/n+1
     def getAn_m(self, n, m):
+        if m == 0 or n == 0:
+            return 1
         if m > n:
             return self.getAn_m(m, n)
         anm = 1
@@ -21,9 +26,16 @@ class Solution:
         n = n + 1
         for i in range(m):
             anm *= n - i
-            amm * = i + 1
+            amm *= i + 1
 
         return anm/amm
 
 
+# test
 s = Solution()
+#assert s.climbStairs(1)  == 1
+#assert s.climbStairs(2)  == 2
+#assert s.climbStairs(3)  == 3
+#assert s.climbStairs(4)  == 5
+#assert s.climbStairs(5)  == 8
+assert s.climbStairs(6)  == 13
