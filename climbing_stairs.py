8ae7fc487bd6528bcea028ec497dd99134e9d122
zhiyelee <zhiyelee@gmail.com>
2015-01-22 08:21:18 +0800

    #70 AC 36ms

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -4,14 +4,32 @@ class Solution:
     def climbStairs(self, n):
         if (n == 1):
             return 1
-        if (n == 2):
-            return 2
+        count = 0
+        for i in range(n + 1):
+            m = n - i
+            if m == 0:
+                count += 1
+            elif m % 2 == 0:
+                count += self.getAn_m(i, m/2)
 
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return count
+
+    # A_m/n+1
+    def getAn_m(self, n, m):
+        if m == 0 or n == 0:
+            return 1
+        if m > n:
+            return self.getAn_m(m, n)
+        anm = 1
+        amm = 1
+        n = m + n
+        for i in range(m):
+            anm *= n - i
+            amm *= i + 1
+
+        return anm/amm
 
 
-
-# test
 s = Solution()
 assert s.climbStairs(1)  == 1
 assert s.climbStairs(2)  == 2
