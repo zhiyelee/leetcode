1651e6c273c7abe1e0742f048cf94b7ec4de5c94
zhiyelee <zhiyelee@gmail.com>
2015-01-30 11:18:52 +0800

    add _util.py include
    `class ListNode`
    `a2ll` array to list
    `pl` print list

---
+++ _util.py
@@ -0,0 +1,22 @@
+
+# test code
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def a2ll(l):
+    ll = ListNode(l[0])
+
+    cursor = ll
+    for i in range(1, len(l)):
+        cursor.next = ListNode(l[i])
+        cursor = cursor.next
+
+    return ll
+def pl(ll):
+    while ll.next:
+        print ll.val,
+        ll = ll.next
+    print ll.val
+
