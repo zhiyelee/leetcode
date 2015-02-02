81b8dc26ef1c53038646e38326956e5f9bafed24
zhiyelee <zhiyelee@gmail.com>
2015-02-02 23:20:54 +0800

    work, but not allowed use `pop` leetcode limit the use of `del, pop(), or remove() `

---
+++ remove_duplicates_from_sorted_array.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param a list of integers
+    # @return an integer
+    def removeDuplicates(self, A):
+        # 1 1 2 2 2 2 3 4 5
+        i = 0
+        val = None
+        while i < len(A):
+            if val == A[i]:
+                A.pop(i)
+            else:
+                val = A[i]
+                i += 1
+
+        print A
+        return len(A)
+
+s = Solution()
+
+assert s.removeDuplicates([1,1,2,2,3,3,4,5,6]) == 6
+assert s.removeDuplicates([1,1]) == 1
+assert s.removeDuplicates([]) == 0
