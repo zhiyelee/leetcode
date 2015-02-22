ee127538d186e87577cd7f2bcc8226607c4b0d15
zhiyelee <zhiyelee@gmail.com>
2015-02-22 20:41:28 +0800

    firt version

--- generate_parentheses.py
+++ generate_parentheses.py
@@ -2,5 +2,28 @@ class Solution:
     # @param an integer
     # @return a list of string
     def generateParenthesis(self, n):
-        return -1
+        if (n <= 0):
+            return []
 
+        arr = []
+        self.g(n, 0, 0, '', arr)
+        return arr
+    def g(self, n, left, right, s, arr):
+        if left < right:
+            return
+
+        if (left == right and left == n):
+            arr.append(s)
+            return
+        if (left == n):
+            self.g(n,left, right + 1, s + ')', arr)
+            return
+
+        self.g(n, left + 1, right, s + '(', arr);
+        self.g(n, left, right + 1, s + ')', arr);
+
+
+s = Solution()
+assert s.generateParenthesis(3) == ["((()))", "(()())", "(())()", "()(())", "()()()"]
+assert s.generateParenthesis(2) == ["(())", "()()"]
+assert s.generateParenthesis(4) != ["(())", "()()"]
