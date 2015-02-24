58744b54e74487e9e56076d1b4f035e151f20a36
zhiyelee <zhiyelee@gmail.com>
2015-02-24 20:30:52 +0800

    Output Limit Exceeded

--- 3sum.py
+++ 3sum.py
@@ -1,4 +1,21 @@
 class Solution:
     # @return a list of lists of length 3, [[val1,val2,val3]]
     def threeSum(self, num):
-        return -1
+        l = len(num)
+        if l < 3:
+            return []
+
+        res = []
+        for i in range(l):
+            for j in range(1, l):
+                for k in range(2, l):
+                    if num[i] + num[j] + num[k] == 0 and num[i] <= num[j] and num[j] <= num[k] and i !=j and j != k:
+
+                        item = [num[i], num[j], num[k]]
+                        if item not in res:
+                            res.append([num[i], num[j], num[k]]);
+        return res
+
+
+s = Solution()
+print s.threeSum([-1, 0, 1, 2, -1, -4])
