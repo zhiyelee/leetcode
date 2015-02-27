2afb334b444b5fe22db0c27f68d8403c8e25d029
zhiyelee <zhiyelee@gmail.com>
2015-02-27 21:51:36 +0800

    Time Limit Exceeded get - O(n) set - O(n)

---
+++ lru_cache.py
@@ -0,0 +1,55 @@
+class LRUCache:
+
+    # @param capacity, an integer
+    def __init__(self, capacity):
+        self.q = []
+        self.capacity = capacity;
+        self.store = {}
+
+
+    # @return an integer
+    def get(self, key):
+        if key in self.store:
+            for i in range(len(self.q)):
+                if self.q[i] == key:
+                    break
+            self.q.pop(i)
+            self.q.append(key)
+            return self.store[key]
+        else:
+            return -1
+
+
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def set(self, key, value):
+        if key in self.store:
+            for i in range(len(self.q)):
+                if self.q[i] == key:
+                    break
+            self.q.pop(i)
+        else:
+            if len(self.q) == self.capacity:
+                discard = self.q.pop(0)
+                del self.store[discard]
+        self.q.append(key)
+        self.store[key] = value
+
+
+s = LRUCache(3)
+s.set(1, 1)
+s.set(2, 2)
+s.set(3, 3)
+s.set(2, 4)
+s.set(4, 5)
+assert s.get(1) == -1
+assert s.get(2) == 4
+
+s = LRUCache(2)
+s.set(2,1)
+s.set(1,1)
+assert s.get(2) == 1
+s.set(4,1)
+assert s.get(1) == -1
+assert s.get(2) == 1
