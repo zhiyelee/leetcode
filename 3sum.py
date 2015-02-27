79e085ac8566947175a633d6d4f5b59b644dd675
zhiyelee <zhiyelee@gmail.com>
2015-02-27 13:30:00 +0800

    AC O(n^2) close #2 sort firt -- O(nlogn) two point iteration with -num[i] as target -- O(n^2)

--- 3sum.py
+++ 3sum.py
@@ -1,21 +1,50 @@
 class Solution:
     # @return a list of lists of length 3, [[val1,val2,val3]]
     def threeSum(self, num):
+        num.sort();
         l = len(num)
         if l < 3:
             return []
 
         res = []
-        for i in range(l):
-            for j in range(i + 1, l):
-                for k in range(i + 2, l):
-                    if num[i] + num[j] + num[k] == 0 and num[i] <= num[j] and num[j] <= num[k]:
+        for i in range(l-2):
+            # avoid duplicate
+            if (i != 0 and num[i] == num[i - 1]):
+                continue
 
-                        item = [num[i], num[j], num[k]]
-                        if item not in res:
-                            res.append([num[i], num[j], num[k]]);
+            left = i + 1
+            right = l - 1
+            target = -num[i]
+
+            while left < right:
+                s = num[left] + num[right]
+                if target == s:
+                    res.append([-target, num[left], num[right]])
+
+                    # avoid duplicate
+                    while left < right:
+                        left += 1
+                        if num[left] > num[left - 1]: break
+
+                    # avoid duplicate
+                    while left < right:
+                        right -= 1
+                        if num[right] < num[right + 1]: break
+
+                elif s < target:
+                    # avoid duplicate
+                    while left < right:
+                        left += 1
+                        if num[left] > num[left - 1]: break
+                else:
+                    # avoid duplicate
+                    while left < right:
+                        right -= 1
+                        if num[right] < num[right + 1]: break
+
         return res
 
 
 s = Solution()
-print s.threeSum([-1, 0, 1, 2, -1, -4])
+assert s.threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
+assert s.threeSum([-1, 0, 0, 0,  1, 2]) == [[-1, 0, 1], [0, 0 ,0]]
