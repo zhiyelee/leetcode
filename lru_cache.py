9e85731084452658879ebfe67b217dc2fea4d2aa
zhiyelee <zhiyelee@gmail.com>
2015-03-01 21:26:04 +0800

    AC O(1) both set and get with doubly linked list

--- lru_cache.py
+++ lru_cache.py
@@ -5,24 +5,15 @@ class LRUCache:
         self.q = []
         self.capacity = capacity;
         self.store = {}
+        self.dbl = DBLinkedList()
 
 
     # @return an integer
     def get(self, key):
         if key in self.store:
-            self.lifttofirst(key)
-            return self.store[key]
-        else:
-            return -1
-
-    def lifttofirst(self, key):
-        # q[i] <==> q[end]
-        for i in range(len(self.q)):
-            if self.q[i] == key:
-                break
-        self.q.pop(i)
-        self.q.append(key)
-
+            self.dbl.lift(self.store[key])
+            return self.store[key].value
+        return -1
 
 
     # @param key, an integer
@@ -30,13 +21,51 @@ class LRUCache:
     # @return nothing
     def set(self, key, value):
         if key in self.store:
-            self.lifttofirst(key)
+            n = self.store[key]
+            n.value = value
+            self.dbl.remove(n)
+            self.dbl.append(n)
         else:
-            if len(self.q) == self.capacity:
-                discard = self.q.pop(0)
-                del self.store[discard]
-            self.q.append(key)
-        self.store[key] = value
+            if self.capacity == len(self.store):
+                k = self.dbl.pop()
+                del self.store[k]
+            n = Node(key, value)
+            self.dbl.append(n)
+            self.store[key] = n
+
+
+class Node:
+    def __init__(self, key, value):
+        self.value = value
+        self.key = key
+        self.pre = None
+        self.next = None
+
+class DBLinkedList:
+    def __init__(self):
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail
+        self.tail.pre = self.head
+
+    def append(self, node):
+        tmp = self.tail.pre
+        tmp.next = node
+        node.pre = tmp
+        node.next = self.tail
+        self.tail.pre = node
+    def remove(self, node):
+        node.pre.next = node.next
+        node.next.pre = node.pre
+    def pop(self):
+        tmp = self.head.next
+        self.head.next = tmp.next
+        tmp.next.pre = self.head
+        return tmp.key
+
+    def lift(self, node):
+        self.remove(node)
+        self.append(node)
 
 
 s = LRUCache(3)
@@ -54,4 +83,8 @@ s.set(1,1)
 assert s.get(2) == 1
 s.set(4,1)
 assert s.get(1) == -1
+assert s.get(2) == 1
+
+s = LRUCache(1)
+s.set(2,1)
 assert s.get(2) == 1
