79257ab809f2e6ce512711a9cfebd471a9933c83
zhiyelee <zhiyelee@gmail.com>
2015-03-02 08:41:08 +0800

    init

---
+++ intersection_of_two_linked_lists.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param two ListNodes
+    # @return the intersected ListNode
+    def getIntersectionNode(self, headA, headB):
+        return -1
