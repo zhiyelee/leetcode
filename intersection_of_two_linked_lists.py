d2cbb6c785b80a742b8cc23214a11dba2f5d4e61
zhiyelee <zhiyelee@gmail.com>
2015-03-02 19:41:28 +0800

    AC  run in O(n) time and use only O(1) memory 1 traverse headA and headB, get the the length lA, lB 2 make sure headB longer than headA 3 traverse headB to (lB - lA), then compare from then on

--- intersection_of_two_linked_lists.py
+++ intersection_of_two_linked_lists.py
@@ -8,4 +8,47 @@ class Solution:
     # @param two ListNodes
     # @return the intersected ListNode
     def getIntersectionNode(self, headA, headB):
-        return -1
+        lA = self.getlen(headA)
+        lB = self.getlen(headB)
+        if lA > lB:
+            tmp = headA
+            headA = headB
+            headB = tmp
+            tmp = lA
+            lA = lB
+            lB = tmp
+
+        pA = headA
+        pB = headB
+        intersection = None
+        for i in range(lB - lA):
+            pB = pB.next
+        while pB:
+            if pB.val != pA.val:
+                intersection = None
+            elif not intersection:
+                intersection = pB
+            pB = pB.next
+            pA = pA.next
+
+        return intersection
+
+    def getlen(self, head):
+        c = head
+        l = 0
+        while c:
+            l += 1
+            c = c.next
+        return l
+
+from _util import *
+s = Solution()
+# list A < list B
+n = s.getIntersectionNode(a2ll([1,2,4,5,6]), a2ll([2,1,3,6,5,6]));
+assert n.val == 5
+# list A > list B
+n = s.getIntersectionNode(a2ll([1,2,4,5,6]), a2ll([4,7,6]));
+assert n.val == 6
+# list A = list B
+n = s.getIntersectionNode(a2ll([4,5,6]), a2ll([4,7,6]));
+assert n.val == 6
