c19fdb33cf3e97fd40bcfdd5d1f366a32fab4f3f
zhiyelee <zhiyelee@gmail.com>
2015-03-26 10:35:06 -0500

    shell problem

---
+++ valid_phone_numbers.bash
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# Read from the file file.txt and output all valid phone numbers to stdout.
+while read num
+do
+    if [[ "$num" =~ ^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]] || [[ "$num" =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]];then
+        echo $num 
+    fi 
+done < file.txt 
