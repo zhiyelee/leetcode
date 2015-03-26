ea061b98d17d9c5cd92204fe989ec0d8cf668c68
zhiyelee <zhiyelee@gmail.com>
2015-03-26 17:32:02 -0500

    sed solution

--- valid_phone_numbers.bash
+++ valid_phone_numbers.bash
@@ -1,9 +1,14 @@
 #! /usr/bin/env bash
 
 # Read from the file file.txt and output all valid phone numbers to stdout.
-while read num
-do
-    if [[ "$num" =~ ^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]] || [[ "$num" =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]];then
-        echo $num
-    fi
-done < file.txt
+# # first version, with while
+# while read num
+# do
+#     if [[ "$num" =~ ^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]] || [[ "$num" =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]];then
+#         echo $num
+#     fi
+# done < file.txt
+
+# sed version
+
+sed -nE '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/p' file.txt
