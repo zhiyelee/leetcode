e3b2236fd6557ced2bc3d9cf8d475b020568b8d8
zhiyelee <zhiyelee@gmail.com>
2015-09-01 16:05:28 -0500

    ugly number: refactor only devide by 2,3,5

--- ugly_number.js
+++ ugly_number.js
@@ -4,17 +4,17 @@
  */
 var isUgly = function(n) {
   if (n <= 0) return false;
-
   if (n === 1) return true;
 
-  var excepts = [2, 3, 5];
-
-  var sqrt = Math.sqrt(n);
-  if (Math.floor(sqrt) === sqrt && !~excepts.indexOf(sqrt)) return false;
-
-  for (var i = Math.floor(sqrt); i > 0; i--) {
-    if (n % i === 0) {
-      if (!~excepts.indexOf(i) || !~excepts.indexOf(n / i)) return false;
+  while (n > 5) {
+    if (n % 5 === 0) {
+      n /= 5;
+    } else if (n % 3 === 0) {
+      n /= 3;
+    } else if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      return false;
     }
   }
 
@@ -24,8 +24,9 @@ var isUgly = function(n) {
 
 var eq = require('assert').equal;
 
+eq(isUgly(1), true)
 eq(isUgly(2), true)
 eq(isUgly(6), true)
-eq(isUgly(7), true)
+eq(isUgly(7), false)
 eq(isUgly(14), false)
 
