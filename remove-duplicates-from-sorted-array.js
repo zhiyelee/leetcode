d42692735df160814728d97b14a037e888be1d89
zhiyelee <zhiyelee@gmail.com>
2015-09-04 10:09:09 -0500

    remove duplicates from sorted array: refactor

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -4,7 +4,7 @@
  */
 var removeDuplicates = function(A) {
 
-    if (A.length <= 1) return A;
+    if (A.length <= 1) return A.length;
 
     var pre;
     for (var i = 0; i < A.length;) {
@@ -15,8 +15,13 @@ var removeDuplicates = function(A) {
             i ++;
         }
     }
+
+    return A.length;
 };
 
+var eq = require('assert').equal;
+
 var t = [1,2,3,4,4,5,5,5,6];
-removeDuplicates(t);
-console.log(t)
+eq(removeDuplicates(t), 6);
+eq(removeDuplicates([]), 0);
+eq(removeDuplicates([1,1,1,1]), 1);
