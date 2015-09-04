70e16411bfb54f493a92ee7462a15f701bad1451
zhiyelee <zhiyelee@gmail.com>
2015-09-03 21:14:58 -0500

    subset: far from first bug free

--- subsets.js
+++ subsets.js
@@ -4,4 +4,43 @@
  */
 var subsets = function(nums) {
 
+  var mem = [];
+  var len = nums.length;
+
+  for (var i = 0; i < len; i++) {
+    var min = nums[i];
+    var idx = i;
+    for (var k = i + 1; k < len; k++) {
+      if (nums[k] < min) {
+        min = nums[k];
+        idx = k;
+      }
+    }
+
+    var tmp = [];
+    mem.forEach(function (item) {
+      // use concat instead of push, to clone item, instead of changing
+      var val = [].concat(item, min);
+      tmp.push(val);
+    });
+
+    // make i the latest smallest one
+    swap(nums, i, idx);
+    mem = mem.concat(tmp);
+    mem.push([min]);
+  }
+
+  mem.push([]);
+  return mem;
 };
+
+function swap(arr, i, j) {
+  var tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
+
+
+var eq = require('assert').equal;
+
+eq(subsets([4, 2, 3]).length, 8)
