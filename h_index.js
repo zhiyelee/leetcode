4c960e2bbc57e7fd28722c5920595dc14f727943
zhiyelee <zhiyelee@gmail.com>
2015-09-05 12:00:00 -0500

    H-index: hash solution, not good enough

--- h_index.js
+++ h_index.js
@@ -2,28 +2,67 @@
  * @param {number[]} citations
  * @return {number}
  */
-var hIndex = function(citations) {
-  if (!citations.length) return 0;
-  citations = citations.sort(function (a, b) {
-    return b - a;
+// var hIndex = function(citations) {
+//   if (!citations.length) return 0;
+//   citations = citations.sort(function (a, b) {
+//     return b - a;
+//   });
+//
+//   var pre = citations[0];
+//   var h = Math.min(pre, 1);
+//   # dummy node at the end
+//   citations.push(-1);
+//
+//   for (var i = 1; i < citations.length; i++) {
+//     var val = citations[i];
+//
+//     if (pre >= i) {
+//       h = i;
+//     }
+//     pre = val;
+//   }
+//
+//   return h;
+// };
+
+// hash solution
+var hIndex = function(cs) {
+  var possible = [];
+  if (cs.length === 0) return 0;
+
+  // possible array length = cs.length + 1, 0 - cs.length
+  possible.length = cs.length + 2;
+  possible = possible.join('-').split('').map(function() {
+    return 0;
   });
 
-  var pre = citations[0];
-  var h = Math.min(pre, 1);
-  # dummy node at the end
-  citations.push(-1);
-
-  for (var i = 1; i < citations.length; i++) {
-    var val = citations[i];
-
-    if (pre >= i) {
-      h = i;
-    }
-    pre = val;
+  for (var i = 0; i < cs.length; i ++) {
+    var val = cs[i];
+    possibleAdd(possible, val);
   }
 
-  return h;
-};
+  return Math.max.apply(null, possible);
+}
+
+// [4,4,4,4]
+// [0, 0, 0 , 0, 0]
+function possibleAdd(arr, val) {
+  var i = 0;
+  while (i < arr.length) {
+    // possible[i] == min(i, val)
+    //if (val > i && val > arr[i]) {
+    //  arr[i] += 1;
+    //}
+    console.log(i,val)
+    if (val > arr[i]) {
+      arr[i] = Math.min(i, arr[i] + 1);
+    }
+
+    i++;
+  }
+  console.log(arr)
+}
+
 
 var eq = require('assert').equal;
 
