1ab1c7b53319f4e9436c56fc62ee76c718581263
zhiyelee <zhiyelee@gmail.com>
2015-09-10 15:33:54 -0500

    Reverse Words in a String: use additional array solution

---
+++ reverse_words_in_a_string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+var reverseWords = function(str) {
+  var words = str.trim().split(/\s+/);
+
+  return words.reverse().join(' ')
+};
+
+var eq = require('assert').equal;
+
+eq(reverseWords(' the sky is blue'), 'blue is sky the');
+eq(reverseWords('the  sky is blue'), 'blue is sky the');
+eq(reverseWords('the  sky is blue!'), 'blue! is sky the');
+eq(reverseWords('the  sky is blue !'), '! blue is sky the');
