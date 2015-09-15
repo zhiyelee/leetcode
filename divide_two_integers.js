232455ee2807f6275bf6f23fabf3a8d378732ba6
zhiyelee <zhiyelee@gmail.com>
2015-09-14 23:58:25 -0500

    fix overflow

--- divide_two_integers.js
+++ divide_two_integers.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var divide = function(dividend, divisor) {
-  if (!divisor) return MAX_INT;
+  if (!divisor) return +Infinity;
   var sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
 
   dividend = Math.abs(dividend);
@@ -14,20 +14,17 @@ var divide = function(dividend, divisor) {
 
   var res;
   var sum = 0;
-  console.log(dividend, divisor)
   while (dividend >= divisor) {
     var val = divisor;
     var ans = 1;
-    while (dividend >= (val << 1)) {
-  console.log(dividend, val << 1, 'loop', ans << 1)
+    // val << 1 may overflow to negative
+    while (dividend >= (val << 1) && (val << 1) > 0) {
       val <<= 1;
       ans <<= 1;
     }
 
-    console.log(val, ans)
     dividend -= val;
     sum += ans;
-    console.log(dividend, divisor)
   }
 
   if (!~sign) sum =  -sum;
@@ -39,4 +36,4 @@ var eq = require('assert').equal;
 
 eq(divide(18, 2), 9);
 eq(divide(32, -2), -16);
-eq(divide(2147483647, 2), -16);
+eq(divide(2147483647, 2), 1073741823);
