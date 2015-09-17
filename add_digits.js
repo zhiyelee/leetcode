8bec0f5f218082b6503fa9c6ce7ea9baa195a801
zhiyelee <zhiyelee@gmail.com>
2015-09-17 11:12:45 -0500

    Add Digits:about digit root https://en.wikipedia.org/wiki/Digital_root

--- add_digits.js
+++ add_digits.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
 var addDigits = function(num) {
+  // https://en.wikipedia.org/wiki/Digital_root
+  return num ? num - 9 * Math.floor((num - 1)/9) : 0;
 
 };
+
+
+var eq = require('assert').equal;
+
+eq(addDigits(0), 0);
+eq(addDigits(18), 9);
+eq(addDigits(21), 3);
