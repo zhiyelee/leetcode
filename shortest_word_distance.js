299736018cc26ed5a3aa7381dfcc49e0723ad441
zhiyelee <zhiyelee@gmail.com>
2015-09-18 14:42:39 -0500

    Shortest Word Distance: much clear and clever solution, with init index = -1

--- shortest_word_distance.js
+++ shortest_word_distance.js
@@ -5,34 +5,22 @@
  * @return {number}
  */
 var shortestDistance = function(words, a, b) {
-  var idxA;
-  var idxB;
-  var min;
+  var idxA = -1;
+  var idxB = -1;
+  var min = Infinity;
 
   words.forEach(function (val, i) {
     if (val === a) {
       idxA = i;
-      if (min) {
-        min = Math.min(min, Math.abs(idxA - idxB));
-      } else {
-        if (idxB !== undefined) {
-          min = Math.abs(idxA - idxB);
-        }
-      }
     }
 
     if (val === b) {
       idxB = i;
-      if (min) {
-        min = Math.min(min, Math.abs(idxB - idxA));
-      } else {
-        if (idxA !== undefined) {
-          min = Math.abs(idxB - idxA);
-        }
-      }
     }
 
-    if (min === 1) return;
+    if (~idxA && ~idxB) {
+      min = Math.min(min, Math.abs(idxA - idxB));
+    }
   });
 
   return min;
