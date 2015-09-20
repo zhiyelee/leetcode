a220702e67930b95a173afbec49f3033b1465fad
zhiyelee <zhiyelee@gmail.com>
2015-09-20 00:17:31 -0500

    Move Zeroes: two pointer solution, solution based on the result, move one item one time to the finally position

--- move_zeroes.js
+++ move_zeroes.js
@@ -3,18 +3,39 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  var slow = -1;
-  var fast = 0;
+  // 1 2 0 3 4 5 0 0  0 6 7
+  var slow = nums.indexOf(0);
+
+  if (!~slow) return;
 
   for (var i = 0; i < nums.length; i++) {
-    var val =  nums[i];
-
-    if (val === 0 && slow === -1) {
-      slow = i;
+    if (nums[i] !== 0 && i > slow) {
+      nums[slow ++] = nums[i];
     }
+  }
 
-    if (val !== 0) {
-      fast = i;
+  if (slow !== -1) {
+    while (slow < nums.length) {
+      nums[slow++] = 0;
     }
   }
 };
+
+
+var eq = require('assert').deepEqual;
+
+var arr = [1, 2, 3, 0, 0 , 0, 4, 1, 0];
+moveZeroes(arr);
+eq(arr, [1,2,3,4,1, 0, 0, 0, 0]);
+
+arr = [1, 2, 3];
+moveZeroes(arr);
+eq(arr, [1,2,3]);
+
+arr = [0, 1, 2, 3];
+moveZeroes(arr);
+eq(arr, [1,2,3, 0]);
+
+arr = [];
+moveZeroes(arr);
+eq(arr, []);
