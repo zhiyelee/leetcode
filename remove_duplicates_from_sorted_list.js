fb425c96a54b71ea12f00287fcd4af649bfd00e7
zhiyelee <zhiyelee@gmail.com>
2015-09-25 08:53:59 -0500

    Remove Duplicates from Sorted List: O(1) space

--- remove_duplicates_from_sorted_list.js
+++ remove_duplicates_from_sorted_list.js
@@ -10,18 +10,16 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-  var hash = new Set();
   var current = head;
   var pre = null;
 
   while (current) {
     var val = current.val;
 
-    if (hash.has(val)) {
+    if (pre && pre.val === val) {
       pre.next = current.next;
     } else {
       pre = current;
-      hash.add(val);
     }
 
     current = current.next;
@@ -34,10 +32,10 @@ var eq = require('assert').deepEqual;
 var nodeList = require('./_util').nodeList;
 var l;
 
-l = nodeList.create([1, 2, 2, 1, 3]);
+l = nodeList.create([1, 2, 2, 3]);
 eq(nodeList.toArray(deleteDuplicates(l)), [1,2,3]);
 
-l = nodeList.create([1, 1]);
+l = nodeList.create([1, 1, 1]);
 eq(nodeList.toArray(deleteDuplicates(l)), [1]);
 l = nodeList.create([]);
 eq(nodeList.toArray(deleteDuplicates(l)), []);
