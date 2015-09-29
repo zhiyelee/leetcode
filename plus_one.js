5fd0f35bac382e5e9516c3639e9b2b362c0b0066
zhiyelee <zhiyelee@gmail.com>
2015-09-28 23:47:53 -0700

    update

--- plus_one.js
+++ plus_one.js
@@ -3,9 +3,9 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    var len = digits.length;
     var carry = 1;
-    for (var i = len - 1; i >= 0; i--) {
+
+    for (var i = digits.length - 1; i >= 0; i--) {
         var val = digits[i];
         digits[i] = (val + carry) % 10;
         carry = Math.floor((val + carry) / 10);
@@ -21,5 +21,5 @@ var plusOne = function(digits) {
 var eq = require('assert').deepEqual;
 
 eq(plusOne([0]), [1]);
-eq(plusOne([1,0, 9]), [1, 1, 0]);
+eq(plusOne([1, 0, 9]), [1, 1, 0]);
 eq(plusOne([9]), [1, 0]);
