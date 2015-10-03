476e72442e54f752b75e675c7d560c6d12873d34
zhiyelee <zhiyelee@gmail.com>
2015-10-02 22:01:06 -0700

    brilliant solution

--- find_the_duplicate_number.js
+++ find_the_duplicate_number.js
@@ -6,4 +6,27 @@ var findDuplicate = function(nums) {
   // 1 2 3 3 4
   // 1 3 3 3 3
   // solution http://keithschwarz.com/interesting/code/?dir=find-duplicate
+  var len = nums.length;
+
+  var slow = len -1;
+  var fast = len - 1;
+  while(true) {
+    slow = nums[low] - 1;
+    fast = nums[nums[fast] - 1] - 1;
+
+    if (slow === falst) {
+       break;
+    }
+  }
+
+  var finder = l - 1;
+  while(true) {
+     slow = nums[slow] - 1;
+     finder = nums[slow] - 1;
+
+     if (slow === finder) {
+       return slow + 1;
+     }
+  }
 };
+
