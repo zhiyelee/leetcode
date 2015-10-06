b4c3b90705d5e49eb17b9637406c6fca429e49bd
zhiyelee <zhiyelee@gmail.com>
2015-10-06 09:15:22 -0700

    Excel Sheet Column Title： AC

--- excel_sheet_column_title.js
+++ excel_sheet_column_title.js
@@ -3,5 +3,23 @@
  * @return {string}
  */
 var convertToTitle = function(n) {
+  // A unicode value 65
+  var s = 65;
+  var ans = '';
+  while (n) {
+    n -= 1;
+    var tmp = String.fromCharCode(s + n % 26)
+    ans = tmp + ans;
 
+    n = Math.floor(n / 26);
+  }
+
+  return ans;
 };
+
+
+var eq = require('assert').equal;
+
+eq(convertToTitle(27), 'AA')
+eq(convertToTitle(26), 'Z')
+eq(convertToTitle(703), 'AAA')
