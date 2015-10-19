59af88d3136b7e3ec4eb804395a79433313f8c1c
zhiyelee <zhiyelee@gmail.com>
2015-10-18 21:09:01 -0700

    Strobogrammatic Number II: use sub function to avoid string substr op

--- strobogrammatic_number_ii.js
+++ strobogrammatic_number_ii.js
@@ -3,5 +3,36 @@
  * @return {string[]}
  */
 var findStrobogrammatic = function(n) {
-
+  return sub(n, n);
 };
+
+function sub(n, m) {
+  if (n === 0) return [''];
+  if (n === 1) return ['0', '1', '8'];
+
+  var tmp = sub(n - 2, m);
+  var res = [];
+
+  tmp.forEach(function (item) {
+    if (n !== m) {
+      res.push('0' + item + '0');
+    }
+
+    res.push('1' + item + '1');
+    res.push('6' + item + '9');
+    res.push('8' + item + '8');
+    res.push('9' + item + '6');
+
+  });
+
+  return res;
+
+}
+
+
+var eq = require('assert').deepEqual;
+
+eq(findStrobogrammatic(2), ["11","69","88","96"]);
+eq(findStrobogrammatic(4), ["11","69","88","96"]);
+//eq(findStrobogrammatic(3), ["101", '111', "69","88","96"]);
+eq(findStrobogrammatic(13), ["101", '111', "69","88","96"]);
