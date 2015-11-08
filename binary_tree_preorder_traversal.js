64c28018fd764c1c803520fcb7d517d4e6217d22
zhiyelee <zhiyelee@gmail.com>
2015-11-08 13:40:23 -0800

    Binary Tree Preorder Traversal: queue

---
+++ binary_tree_preorder_traversal.js
@@ -0,0 +1,37 @@
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  var stack = [];
+
+  if (!root) return [];
+
+  stack.push(root);
+
+  var it;
+  var ans = [];
+  while(stack.length) {
+    it = stack.shift();
+    if (it.val === null) continue;
+
+    ans.push(it.val);
+
+    if (it.right) {
+      stack.unshift(it.right);
+    }
+    if (it.left) {
+       stack.unshift(it.left);
+    }
+
+  }
+
+  return ans;
+};
+
+var Tree = require('leetcode').Tree;
+var eq = require('assert').deepEqual;
+
+eq(preorderTraversal(Tree.create([1,null,2,3])), [1,2,3]);
+eq(preorderTraversal(Tree.create([1,null,2,3,4, null, 5])), [1,2,3, 5, 4]);
+
