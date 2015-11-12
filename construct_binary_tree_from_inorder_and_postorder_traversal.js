73ce0b0106a40a177ecc81644db8b57986355a95
zhiyelee <zhiyelee@gmail.com>
2015-11-11 20:35:39 -0800

    Construct Binary Tree from Inorder and Postorder Traversal: Memory Limit Exceeded

---
+++ construct_binary_tree_from_inorder_and_postorder_traversal.js
@@ -0,0 +1,43 @@
+var Tree = require('leetcode').Tree;
+var TreeNode = Tree.node;
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function(inorder, postorder) {
+  // 4 2 1 3 5
+  // 4 2 5 3 1
+  //
+
+  var parent = {};
+  build(inorder, postorder, parent);
+
+  return parent.next;
+};
+
+function build(inorder, postorder, parent, type) {
+  if (!inorder.length || inorder.length !== postorder.length) return null;
+
+  var val = postorder.pop();
+  var root = new TreeNode(val);
+  var idx = inorder.indexOf(val);
+  if (type) {
+    parent[type] = root;
+  } else {
+    parent.next = root;
+  }
+
+
+  if (!~idx) return null;
+
+  build(inorder.slice(0, idx), postorder.slice(0, idx), root, 'left');
+  build(inorder.slice(idx + 1), postorder.slice(idx), root, 'right');
+}
+
+var eq = require('assert').deepEqual;
+
+var t1 = buildTree([4, 2, 1, 3, 5], [4, 2, 5, 3, 1]);
+var t2 = Tree.create([1, 2, 3, 4, null, null, 5]);
+console.log(t1, t2)
+eq(t1, t2)
